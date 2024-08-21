43545305d7826555fcb33bd70531a2cc7f283d98
Malickmimd <malickdiene@esp.sn>
2024-08-21 23:21:31 +0000

    make the root app route  "/" and the login page route  "/connexion"

--- src/guards/isAuth.tsx
+++ src/guards/isAuth.tsx
@@ -2,18 +2,15 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { useEffect } from "react";
 
+export default function IsAuth() {
+  const { isLogged } = useAuth();
+  const navigate = useNavigate();
 
-export default function IsAuth () {
+  useEffect(() => {
+    if (1 == 1 || isLogged()) {
+      navigate("/");
+    }
+  }, [isLogged, navigate]);
 
-    const { isLogged } = useAuth();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (1 == 1 || isLogged()) {
-            navigate('/app');
-        }
-    }, [isLogged, navigate]);
-
-    
-    return <Outlet />
+  return <Outlet />;
 }
