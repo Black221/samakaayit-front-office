1818cad26a68fb719b444325a8b78f93115f7cc3
Malickmimd <malickdiene@esp.sn>
2024-08-25 01:06:34 +0000

    create RVItem, RVList and Dashboard page

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,9 +1,10 @@
-
-
-
 export type UserModel = {
-    id: string;
-    email: string;
-    role: string;
-}
+  id: string;
+  email: string;
+  role: string;
+};
 
+export type RVModel = {
+  name: string;
+  time: string;
+};
