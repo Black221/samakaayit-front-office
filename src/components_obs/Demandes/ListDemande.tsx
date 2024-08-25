1a28a42a32dff85b9dc3c23364e2d1091378627f
codlabsn <abdoulayeleom10@gmail.com>
2024-08-25 13:37:52 +0000

    Update listDemande divider style

--- src/components_obs/Demandes/ListDemande.tsx
+++ src/components_obs/Demandes/ListDemande.tsx
@@ -2,10 +2,10 @@ import DemandeItem from './DemandeItem'
 
 export default function ListDemande({ demandes }: any) {
   return (
-      <ul className='py-6 px-4'>
+      <ul className='py-6 px-4 divide-y divide-[#7B7C7E] w-full'>
         {
           demandes.map((demande: any) => (
-              <li key={demande.numDossier} className="border-b-[0.3px] border-[#7B7C7E]"><DemandeItem status={demande.status} name={demande.name} numDossier={demande.numDossier} date={demande.date} /></li>
+              <li key={demande.numDossier} className=""><DemandeItem status={demande.status} name={demande.name} numDossier={demande.numDossier} date={demande.date} /></li>
           ))
         } 
       </ul>
