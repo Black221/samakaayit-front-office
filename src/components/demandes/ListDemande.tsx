ccde65ddd413cf9acc1ccc374c734f4f4bf98ab4
codlabsn <abdoulayeleom10@gmail.com>
2024-08-28 17:32:29 +0000

    Add goback button to detail page and fix merging conflicts

---
+++ src/components/demandes/ListDemande.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import { DemandeModel } from "../../types/models";
+import DemandeItem from "./DemandeItem";
+
+interface ListDemandeProps {
+  demandes: DemandeModel[];
+  onClick:  React.MouseEventHandler<HTMLButtonElement> | undefined;
+}
+
+export default function ListDemande({ demandes, onClick }: ListDemandeProps) {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <ul className="py-6">
+        {demandes.map((demande) => (
+          <li
+            key={demande.numDossier}
+            className="border-b-[0.3px] border-[#7B7C7E]"
+          >
+            <DemandeItem demande={demande} onClick={onClick} />
+          </li>
+        ))}
+      </ul>
+      {demandes.length > 7 && (
+        <button
+          onClick={() => navigate("/demandes")}
+          className="mt-4 text-sm text-primary-700 hover:underline"
+        >
+          Voir tout
+        </button>
+      )}
+    </div>
+  );
+}
+
